bd267c89fae5f0adcd154693d9b863ac7ee458d7
lostd <lostd@2f30.org>
2016-05-24 21:44:35 +0100

    Reorder defaults

--- spoon.c
+++ spoon.c
@@ -31,11 +31,11 @@ struct ent {
 	/* reorder this if you want */
 	{ .fmt = "[%s] ", .read = mpdread },
 	{ .fmt = "[%s] ", .read = mixread },
-	{ .fmt = "[%s] ", .read = xkblayoutread },
 	{ .fmt = "[%s] ", .read = cpuread },
 	{ .fmt = "[%s] ", .read = tempread },
 	{ .fmt = "%s ", .read = battread },
 	{ .fmt = "%s ", .read = wifiread },
+	{ .fmt = "[%s] ", .read = xkblayoutread },
 	{ .fmt = "%s", .read = dateread },
 };
 
