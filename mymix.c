1ba7cf2d05b0d4fe21a76a6da9268228fa82f05c
RetoranPetra <flyro@live.co.uk>
2023-04-03 00:04:35 +0100

    Got it working, problem was in it needing to be installed.

--- mymix.c
+++ mymix.c
@@ -1,22 +1,21 @@
-#include <err.h>
 #include <stdio.h>
-
-#include "types.h"
-#include "util.h"
 
 int
 mymixread(void *arg, char *buf, size_t len) {
 	FILE *fp;
-	static char outBuffer[8] = {};
+	static char outBuffer[16] = {};
 	//Open pulsemixer file.
-	
 	fp = popen("/usr/bin/pulsemixer --get-volume", "r");
 	if (fp == NULL) {
-		warn("popen fail!");
 		return -1;
 	}
 	fgets(outBuffer, sizeof(outBuffer), fp);
-	warn("OutBuffer: %s",outBuffer);
+	//Checking for 100 100
+	//						 0123456
+	//Checking for 40 40
+	for (int i = 0;i<sizeof(outBuffer);i++) {
+		outBuffer[i] = (outBuffer[i] == '\n') ? '\0' : outBuffer[i];
+	}
 	//snprintf(buf,len,"A!\n");
 /*
 	int i = 0;
@@ -28,8 +27,7 @@ mymixread(void *arg, char *buf, size_t len) {
 	}
 */
 	pclose(fp);
-	snprintf(buf,len,"%s",outBuffer);
 	fflush(fp);
-	
+	snprintf(buf,len,"%s",outBuffer);
 	return 0;
 }
