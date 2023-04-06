2cdbf8455630a52e101ae59c284d30118f3d50ee
RetoranPetra <flyro@live.co.uk>
2023-04-06 16:45:45 +0100

    Whitespace change.

--- batt.c
+++ batt.c
@@ -23,7 +23,7 @@ battprint(char *buf, size_t len, int acon, int life)
 	else if (life >= 60)
 		snprintf(buf, len, "=[%c///]", c);
 	else if (life >= 40)
-		snprintf(buf, len, "=[%c%c/]", c, c);
+		snprintf(buf, len, "=[%c%c//]", c, c);
 	else if (life >= 20)
 		snprintf(buf, len, "=[%c%c%c/]", c, c, c);
 	else
